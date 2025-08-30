c163c06899a4a2f332c316dc9f5c0df427d8bbe3
kimyeonwoo <ppupy1209@naver.com>
2025-08-30 09:46:03 +0900

    Comment CUD API

---
+++ service/comment/src/main/java/board/comment/controller/CommentController.java
@@ -0,0 +1,31 @@
+package board.comment.controller;
+
+import board.comment.service.CommentService;
+import board.comment.service.request.CommentCreateRequest;
+import board.comment.service.response.CommentResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+public class CommentController {
+    private final CommentService commentService;
+
+    @GetMapping("/v1/comments/{commentId}")
+    public CommentResponse read(
+            @PathVariable Long commentId
+    ) {
+        return commentService.read(commentId);
+    }
+
+    @PostMapping("/v1/comments")
+    public CommentResponse create(@RequestBody CommentCreateRequest request) {
+        return commentService.create(request);
+    }
+
+    @DeleteMapping("/v1/comments/{commentId}")
+    public void delete(@PathVariable Long commentId) {
+        commentService.delete(commentId);
+    }
+
+}
