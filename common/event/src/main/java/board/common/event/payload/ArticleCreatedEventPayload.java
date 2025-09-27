8e23dea7fef3387136c60def395a1a7464955b3a
kimyeonwoo <ppupy1209@naver.com>
2025-09-27 10:50:42 +0900

    Event, data-serializer

---
+++ common/event/src/main/java/board/common/event/payload/ArticleCreatedEventPayload.java
@@ -0,0 +1,22 @@
+package board.common.event.payload;
+
+import board.common.event.EventPayload;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class ArticleCreatedEventPayload implements EventPayload {
+    private Long articleId;
+    private String title;
+    private String content;
+    private Long boardId;
+    private Long writerId;
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
+    private Long boardArticleCount;
+}
